7df28dc731868ad01fb54a9b7d1cf17eea796869
Ziye <zzhang2293@outlook.com>
2023-04-25 22:19:22 -0500

    add a function helps get the name, not complete

--- p7/P7/starter_code/runscan.c
+++ p7/P7/starter_code/runscan.c
@@ -6,6 +6,34 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+void read_directory_data_blocks(int fd, struct ext2_inode inode, struct ext2_super_block super)
+{
+
+    int num_blocks = (inode.i_size + block_size - 1) / block_size;
+    for (int i = 0; i < EXT2_N_BLOCKS; i ++) // ext2_n_block is max number of block in inode
+    {
+        if (inode.i_block[i] != 0){
+            int offset = 0;
+            while (offset < block_size && num_blocks > 0)
+            {
+                char buffer[block_size];
+
+                int read_size = block_size;
+                pread(fd, buffer, read_size, inode.i_block[i] * block_size + offset);
+                struct ext2_dir_entry_2 *entry = (struct ext2_dir_entry_2 *) buffer;
+                while((char *) entry < buffer + read_size){
+                    char name[EXT2_NAME_LEN + 1];
+
+                    // perhaps get the name from the entry
+
+                }
+            }
+            
+        }
+    }
+}
+
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -59,7 +87,6 @@ int main(int argc, char **argv)
     ext2_group_desc group;
     off_t inode_offset;
     int file_name_number = 1;
-
     int i_num = 1; // track which inode num this is
     int count = 0; // for test case
     // printf("hello world 60\n");
