481b621b83f609e7d911621a91d514ad443348ce
Daniel Kulbachenko <kulbachenko@royal-05.cs.wisc.edu>
2023-04-26 16:28:35 -0500

    single indirect working

--- p7/P7/starter_code/runscan.c
+++ p7/P7/starter_code/runscan.c
@@ -6,32 +6,34 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void read_directory_data_blocks(int fd, struct ext2_inode inode, struct ext2_super_block super)
-{
+// void read_directory_data_blocks(int fd, struct ext2_inode inode, struct ext2_super_block super)
+// {
 
-    int num_blocks = (inode.i_size + block_size - 1) / block_size;
-    for (int i = 0; i < EXT2_N_BLOCKS; i++) // ext2_n_block is max number of block in inode
-    {
-        if (inode.i_block[i] != 0)
-        {
-            int offset = 0;
-            while (offset < block_size && num_blocks > 0)
-            {
-                char buffer[block_size];
+//     int num_blocks = (inode.i_size + block_size - 1) / block_size;
+//     for (int i = 0; i < EXT2_N_BLOCKS; i++) // ext2_n_block is max number of block in inode
+//     {
+//         if (inode.i_block[i] != 0)
+//         {
+//             int offset = 0;
+//             while (offset < block_size && num_blocks > 0)
+//             {
+//                 char buffer[block_size];
 
-                int read_size = block_size;
-                pread(fd, buffer, read_size, inode.i_block[i] * block_size + offset);
-                struct ext2_dir_entry_2 *entry = (struct ext2_dir_entry_2 *)buffer;
-                while ((char *)entry < buffer + read_size)
-                {
-                    char name[EXT2_NAME_LEN + 1];
+//                 int read_size = block_size;
+//                 pread(fd, buffer, read_size, inode.i_block[i] * block_size + offset);
+//                 struct ext2_dir_entry_2 *entry = (struct ext2_dir_entry_2 *)buffer;
+//                 while ((char *)entry < buffer + read_size)
+//                 {
+//                     char name[EXT2_NAME_LEN + 1];
 
-                    // perhaps get the name from the entry
-                }
-            }
-        }
-    }
-}
+//                     // perhaps get the name from the entry
+//                 }
+//             }
+//         }
+//     }
+// }
+
+ext2_group_desc *groups;
 
 int main(int argc, char **argv)
 {
@@ -50,7 +52,7 @@ int main(int argc, char **argv)
     ext2_read_init(fd);
 
     struct ext2_super_block super;
-    ext2_group_desc *groups = malloc(sizeof(ext2_group_desc *) * num_groups);
+    groups = malloc(sizeof(ext2_group_desc) * num_groups);
     read_super_block(fd, &super);
     read_group_descs(fd, groups, num_groups);
 
@@ -68,39 +70,49 @@ int main(int argc, char **argv)
 
     // example read first the super-block and group-descriptor
 
-    int inode_size = super.s_inode_size; // inode size
+    // int inode_size = super.s_inode_size; // inode size
     // int inode_total = super.s_inodes_count; // total number of inodes in the file system.
     off_t inode_starter;
     // off_t inode_starter = locate_inode_table(0, &group);    // change to first element of groups?
     // off_t inode_starter = locate_inode_table(0, &groups); // change to first element of groups?
-    off_t inode_table;
+    // off_t inode_table;
 
     // if the dir does not exist then create it
-    char output_dir[] = "output";
+    // char output_dir[] = "output";
+
     struct stat st = {0};
-    if (stat(output_dir, &st) == -1)
-        mkdir(output_dir, 0700);
+    if (stat(argv[2], &st) == -1)
+        mkdir(argv[2], 0700);
+
+    // if (stat(output_dir, &st) == -1)
+    //     mkdir(output_dir, 0700);
 
     // ext2_group_desc group;
-    off_t inode_offset;
+
+    // off_t inode_offset;
+
     // int file_name_number = 1;
-    int file_name_number = 0;
+    int file_name_number = 1;
     // int i_num = 1; // track which inode num this is
     int count = 0; // for test case
     // printf("hello world 60\n");
     // printf("num of groups: %d\n", num_groups);
+
+    // FILE *output_file;
+
+    // char output_filename[100] = ;
+    printf("new file name: %s", "test");
+
     for (int i = 0; i < num_groups; i++)
     {
         inode_starter = locate_inode_table(i, groups);
-        inode_table = lseek(fd, inode_starter, SEEK_SET);
 
         for (int j = 0; j < (int)super.s_inodes_per_group; j++)
         {
-            inode_offset = inode_table + (inode_size * i);
-            lseek(fd, inode_offset, SEEK_SET);
+            // inode_offset = inode_table + (inode_size * j);
             struct ext2_inode inode;
-            read_inode(fd, inode_offset, j, &inode, super.s_inode_size);
-            // printf("hello world");
+            read_inode(fd, inode_starter, j + 1, &inode, super.s_inode_size);
+
             uint32_t block_index = inode.i_block[0];
             if (block_index != 0)
             {
@@ -121,60 +133,51 @@ int main(int argc, char **argv)
                     is_jpg = 1;
                 }
 
-                file_name_number++;
                 if (is_jpg == 1)
                 {
+                    FILE *output_file;
+                    printf("found jpg with inode num %d\n", file_name_number);
                     count++;
                     char output_filename[100];
-                    sprintf(output_filename, "output/file-%d.jpg", file_name_number);
+                    sprintf(output_filename, "%s/file-%d.jpg", argv[2], file_name_number);
                     // file_name_number++;
-                    FILE *output_file;
                     output_file = fopen(output_filename, "w");
                     if (output_file == NULL)
-                        printf("the output file is null");
-                    // find out file size
+                        printf("the output file is null\n");
+
                     printf("output file name is: %s\n", output_filename);
                     int file_size = inode.i_size;
-                    printf("file size is: %d\n", file_size);
+                    printf("file size is: %d bytes\n", file_size);
 
-                    int blocks_required = file_size / block_size;
+                    int blocks_required = (file_size + file_size % block_size) / block_size;
+                    printf("blocks required: %d\n", blocks_required);
 
                     for (int b = 0; b < blocks_required; b++)
                     {
                         if (b <= 11)
                         {
-                            block_index = inode.i_block[0];
+                            block_index = inode.i_block[b];
+                            printf("block index: %d\n", block_index);
                         }
                         else
                         {
                             // need to read indirect block
                             off_t indirect_off = inode.i_block[12] * block_size;
                             pread(fd, buffer, block_size, indirect_off);
-                            block_index = *(buffer + (4 * (b - 12)));
+                            block_index = *(int *)(buffer + (4 * (b - 12)));
                         }
 
                         // now that we have block number, copy that block into output file
+
                         offset = block_index * block_size;
                         pread(fd, buffer, block_size, offset);
                         fwrite(buffer, sizeof(char), block_size, output_file);
                     }
-
-                    // go through inode block pointers based on size
-                    // int num_blocks = (file_size + block_size - 1) / block_size;
-                    // for (int k = 0; k < num_blocks; k ++){
-                    //     uint32_t b = inode.i_block[k];
-                    //     if (b != 0){
-                    //         off_t each_block_offsite = block_index * block_size;
-                    //         char buffer[block_size];
-                    //         pread(fd, buffer, block_size, each_block_offsite);
-                    //         // write each relevant block to file
-                    //         fwrite(buffer, sizeof(char), block_size, output_file);
-
-                    //     }
-                    //}
-                    // fclose(output_file);
+                    fclose(output_file);
                 }
             }
+
+            file_name_number++;
         }
     }
     printf("count is: %d\n", count);
